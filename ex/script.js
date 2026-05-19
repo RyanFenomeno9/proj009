87d6bcaf802009e4465bd4ecd290098c642608ea
favoreto.ryan@gmail.com <favoreto.ryan@gmail.com>
2026-05-19 10:52:47 -0300

    fdfqfghyjukilo

---
+++ ex/script.js
@@ -0,0 +1,11 @@
+let alunos = [{nome: "Luan", Idade: 16, DisciplinaFavorita: "Plug"},
+    {nome: "Murilo", Idade: 16, DisciplinaFavorita: "Tips"},
+    {nome: "Breno", Idade: 16, DisciplinaFavorita: "Sals"},
+    {nome: "Vinicius", Idade: 16, DisciplinaFavorita: "Allan"},
+    {nome: "Wesley", Idade: 16, DisciplinaFavorita: "FIAP"}
+];
+
+
+alert(`${alunos[0].nome}`);
+alert(`${alunos[4].DisciplinaFavorita}`);
+
